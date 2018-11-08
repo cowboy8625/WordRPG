94ba38f6980e1dd4d24fe1c664114df3cd277e9f
xizes2 <40306898+xizes2@users.noreply.github.com>
2018-11-08 15:47:11 -0200

    Update Mobs.py
    Created 2 more classes (after Golem), witches and hell wolfes

--- Mobs.py
+++ Mobs.py
@@ -50,3 +50,38 @@ class Golem:
         self.deffence = 0
         self.pures = 0
         self.luck = 0
+        
+##-- Witches are magical creatures, they are not physically strong but can invoke spell to make damage or confuse the player, making him lose a turn. --##
+
+class Witch:
+
+    def __init__(self):
+    
+        self.max_health = 100
+        self.heath = 100
+        self.armor = 0
+        self.melee_attack = 0
+        self.magic_attack = 50
+        self.mama = 0
+        self.stamina = 0
+        self.deffence = 0
+        self.pures = 0
+        self.luck = 0
+
+
+##-- Hell Wolfes are wolfes speled by evil creatures. They are weak, but really fast.
+
+class Hell_Wolfes:
+
+    def __init__(self):
+    
+        self.max_health = 80
+        self.heath = 80
+        self.armor = 0
+        self.melee_attack = 0
+        self.magic_attack = 0
+        self.mama = 0
+        self.stamina = 0
+        self.deffence = 0
+        self.pures = 0
+        self.luck = 0
