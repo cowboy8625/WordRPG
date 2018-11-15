43e832fa08f5eb47119d45721de3e5b5555de799
BJTMastermind <bjtmastermind@yahoo.com>
2018-11-15 16:21:17 -0500

    Updated Changelog
    Updated Changelog to have a more official design.

--- ChangeLog.py
+++ ChangeLog.py
@@ -1,6 +1,5 @@
 import Screen
-
 def change_log():
-    changelog1 = 'Test'
-    Screen.display(changelog1)
+    changelog = "  ::Changlog for pre-beta v0.1::\t+Added this changelog to the game"
+    Screen.display(changelog, width=70)
 change_log()
