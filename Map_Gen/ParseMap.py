409d9178a80cf46448bf187f40d331025adc2141
PinkChicken <jorellcroft4@gmail.com>
2018-12-19 22:08:01 -0700

    Adding Node Keys
    Biome.py: Creating a dictionary holding all the node "Keys"
    
    ParseMap.py: importing The newly made node dictionary
    Sudocoded a populate function

--- Map_Gen/ParseMap.py
+++ Map_Gen/ParseMap.py
@@ -1,8 +1,15 @@
-
+from Map_Gen import Biome
 
 class Map:
     def __init__(self, map_file):
         self.map = open(map_file, 'r')
+    """
+    def populate(self):
+        __map = self.parse()
+        for y in range(len(__map)):
+            for x in range(len(__map)):
+                node = __map[x][y]
+    """
 
     def parse(self):
         __map = list(self.map.read().split())
@@ -11,5 +18,10 @@ class Map:
             __map[i] = list(__map[i])
 
         return __map
+
+
+mp = Map("World.map")
+mp.populate()
+
 
 
