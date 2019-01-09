b97cdbdc93fde184baa8bd20f96f4f6fb76eb973
Christopher L Bruce <37425633+ChristopherLBruce@users.noreply.github.com>
2019-01-09 11:55:40 -0600

    Update __init__.py
    explanded docstring

--- WordRPG/gui/__init__.py
+++ WordRPG/gui/__init__.py
@@ -1,3 +1,19 @@
-""" gui module """
+""" gui module
+
+  The gui module works by creating and compositing 2D arrays of indivudal string
+  characters called a 'screen'.
+  
+  The maximum dimensions of a screen is 80 columns x 30 rows, which matches the
+  terminal output window.
+
+  Screens can also be parsed in from plain text files saved in the 'screens'
+  sub-folder.
+
+  Screens can be composited together or have indicies directly overwritten
+
+  Once a final screen array has been created, it is converted to a single string and then
+  written to the terminal output window.
+
+"""
 
 from . import const, main, screen
