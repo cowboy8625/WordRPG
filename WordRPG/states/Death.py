6983f9c2ba4cbd44d0c54d628cced26032dd0bdb
ChristopherLBruce <37425633+ChristopherLBruce@users.noreply.github.com>
2019-01-20 12:07:09 -0600

    placeholder death screen

--- WordRPG/states/Death.py
+++ WordRPG/states/Death.py
@@ -10,13 +10,20 @@ class Death(State):
         """ Initiailize class and super class """
         super(Death, self).__init__()
         self.first_time = True
+        self.screen = self._init_screen()
 
+
+    def _init_screen(self):
+        screen = Screen()
+        screen.load_screen('tombstone', offset=('center',2), fgcolor='WHITE')
+        screen.add_string_to_screen('YOU ARE DEAD', offset=('center', 27),
+                    fgcolor='RED')
+        return screen
+        
 
     def update_screen(self):
         """ Draws the screen """
-        Screen.clear()
-        print('THIS IS A PLACEHOLDER SCREEN FOR DEATH STATE')
-        print('PRESS KEY TO RETURN TO GAME STATE')
+        self.screen.draw()
 
 
     def on_event(self, event, prev_state):
