d8622338f839f8192f77b11a4a1de420c8b20928
ChristopherLBruce <37425633+ChristopherLBruce@users.noreply.github.com>
2019-01-21 19:54:37 -0600

    Map module and classes for generating map array

---
+++ test_map.py
@@ -0,0 +1,14 @@
+from WordRPG.Map import Map
+from WordRPG.Map.tiles import BIOMES
+
+test_map = Map(r"E:\Python\WordRPG\WordRPG\Map\images\test_island1.png")
+# print(test_map.map_key)
+# print(test_map.map[0][0])
+test_map.show()
+
+
+# color_map = { v['img_color'] : k  for k, v in BIOMES.items() }
+# print(color_map)
+
+while True:
+    pass
