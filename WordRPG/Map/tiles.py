bff6cd80de06f90d9a76de7c4fa3a37b49ebd51f
Chris Bruce <chris.bruce@dsvolition.com>
2019-01-22 14:16:59 -0600

    tiles are now defined using a 'Tile' class

--- WordRPG/Map/tiles.py
+++ WordRPG/Map/tiles.py
@@ -1,170 +1,169 @@
+""" Module for Tile class and tilesets """
+
 from PIL import ImageColor
 
-""" Module for formatting text
+from ..gui.const import DEF_FORMAT
 
-    Notes:
-        Fore/Back:
-            'BLACK', 'BLUE', 'CYAN', 'GREEN', 'LIGHTBLACK_EX', 'LIGHTBLUE_EX',
-            'LIGHTCYAN_EX', 'LIGHTGREEN_EX', 'LIGHTMAGENTA_EX', 'LIGHTRED_EX',
-            'LIGHTWHITE_EX', 'LIGHTYELLOW_EX', 'MAGENTA', 'RED', 'RESET',
-            'WHITE', 'YELLOW'
-        Style:
-            'BRIGHT', 'DIM', 'NORMAL', 'RESET_ALL'
+
+
+class Tile:
+    """
+    Base class for a Map Tile
+
+    **Arguments:**
+
+        None
+
+    **Keword Arguments:**
+
+        :``name``:          `str`   name of the tile
+        :``resources``:     `list`  list of resources that can be gathered from
+                                    this tile. Default is None
+        :``movement``:      `int`   value that represents how difficult it is
+                                    to move into this tile. Higher numbers are
+                                    more hazardous tiles. Value of -1 means the
+                                    tile is impassable. Default value is 1
+        :``color``:         `str`   name of a pre-defeind HTML color. color is
+                                    used to build a color map key for loading
+                                    map images and converting pixels to tile
+                                    data. Default is 'blue'
+                            `tuple` (r, g, b) color value. 0-255 range.
+        :``symbol``:        `str`   character used when drawing the map
+        :``description``:   `str`   description of the tile
+        :``format``:        `dict`  defines how tile is drawn to screen
+        :``discovered``:    `bool`  If True, tile is drawn. Default is False
+
+    """
+
+    # define class slots to improve memory effeciency
+    # http://book.pythontips.com/en/latest/__slots__magic.html
+    __slots__ = ['name', 'resources', 'movement', 'color', 'symbol',
+                 'description', 'format', 'discovered']
+
+    def __init__(self, name='TILE', resources=None, movement=1,
+                 color='blue', symbol=' ',
+                 description='[TILE DESCRIPTION]',
+                 _format=DEF_FORMAT
+                 ):
+        self.name = name
+        self.resources = resources
+        self.movement = movement
+        self.color = self._get_rgb_color(color)
+        self.description = description
+        self.symbol = symbol
+        self.format = _format
+        self.discovered = False
+
+
+    def _get_rgb_color(self, color):
+        """ get rgb color value from color name or tuple """
+        if isinstance(color, str):
+            return ImageColor.getrgb(color)
+
+        return color
+
+
+    def __repr__(self):
+        """ returns representation of Tile object """
+        attrs = [f'{s}={getattr(self, s)}' for s in self.__slots__]
+        return f'Tile({", ".join(attrs)})\n'
+
+
+    def __str__(self):
+        """ returns printable str for tile """
+        return self.symbol
 
 """
-
-
-
-""" Diciontaries for various tilesets are defined here
-
-    '[tile_name]': {[dict]}
-
-    'resource': {list} - list of resources that can be found in this tile
-    'movement': {int} - movement cost to move into tile.
-                    -1 means tile is
-                    impassable
-    'description': 'YOU ARE ON THE ROAD',
-    'img_color' : (R, G, B) color used in source image
-    'symbol' : Ascii symbol used to draw tile in game window
-    'format' : {[dict]}
-        'fgcolor':'BLACK'
-        'bgcolor':'YELLOW'
-        'style':'NORMAL'
+format key options
+    fgcolor/bgcolor:
+        'BLACK', 'BLUE', 'CYAN', 'GREEN', 'LIGHTBLACK_EX', 'LIGHTBLUE_EX',
+        'LIGHTCYAN_EX', 'LIGHTGREEN_EX', 'LIGHTMAGENTA_EX', 'LIGHTRED_EX',
+        'LIGHTWHITE_EX', 'LIGHTYELLOW_EX', 'MAGENTA', 'RED', 'RESET',
+        'WHITE', 'YELLOW'
+    style:
+        'BRIGHT', 'DIM', 'NORMAL', 'RESET_ALL'
 """
 
 BIOMES = {
-    'village': {
-        'resource': None, 
-        'movement': 1 ,     # roads are the fastest/safest way to travel
-        'description': 'YOU ARE IN A VILLAGE',
-        'img_color' : ImageColor.getrgb('red'),
-        'symbol' : '±',
-        'format' : {'fgcolor':'BLACK','bgcolor':'WHITE','style':'NORMAL'},
-        },
-    'cave': {
-        'resource': None, 
-        'movement': 1 ,     # roads are the fastest/safest way to travel
-        'description': 'YOU ARE STANDING AT THE ENTRANCE TO A CAVE',
-        'img_color' : ImageColor.getrgb('black'),
-        'symbol' : '▄',
-        'format' : {'fgcolor':'BLACK','bgcolor':'LIGHTBLACK_EX','style':'NORMAL'},
-        },
-    ## -------------------------------------------------------------------------
-    ## passable biomes
-    ## -------------------------------------------------------------------------    
-    'road': {
-        'resource': None, 
-        'movement': 1 ,     # roads are the fastest/safest way to travel
-        'description': 'YOU ARE ON THE ROAD',
-        'img_color' : ImageColor.getrgb('white'),
-        'symbol' : '·',
-        'format' : {'fgcolor':'BLACK','bgcolor':'LIGHTBLACK_EX','style':'NORMAL'},
-        },
-    'farmland': {
-        'resource' : ['herb'], 
-        'movement' : 2 ,
-        'description' : 'YOU ARE ON FARMLAND',
-        'img_color' : ImageColor.getrgb('magenta'),
-        'symbol' : '≡',
-        'format' : {'fgcolor':'LIGHTYELLOW_EX','bgcolor':'GREEN','style':'NORMAL'},   
-        },
-    'beach': {
-        'resource' : None, 
-        'movement' : 2 ,
-        'description' : 'YOU ARE ON THE BEACH',
-        'img_color' : ImageColor.getrgb('khaki'),
-        'symbol' : '░',
-        'format' : {'fgcolor':'WHITE','bgcolor':'YELLOW','style':'NORMAL'},   
-        },
-    'desert': {
-        'resource' : ['grass'], 
-        'movement' : 3 ,
-        'description' : 'YOU ARE IN THE DESERT',
-        'img_color' : (205, 133, 63),   #ImageColor.getrgb('peru'),
-        'symbol' : '░',
-        'format' : {'fgcolor':'RED','bgcolor':'YELLOW','style':'NORMAL'},   
-        },
-    'grassland': {
-        'resource' : ['grass'], 
-        'movement' : 2 ,
-        'description' : 'YOU ARE IN GRASSLANDS',
-        'img_color' : ImageColor.getrgb('lawngreen'),
-        'symbol' : ' ',
-        'format' : {'fgcolor':'LIGHTYELLOW_EX','bgcolor':'LIGHTGREEN_EX','style':'NORMAL'},   
-        },
-    'forest': {
-        'resource': ['wood'], 
-        'movement': 3 ,
-        'description': 'YOU ARE IN THE FOREST',
-        'img_color' : ImageColor.getrgb('olivedrab'),
-        'symbol' : '♣',     #'▒',
-        'format' : {'fgcolor':'GREEN','bgcolor':'LIGHTGREEN_EX','style':'NORMAL'},
-        },
-    'deep_forest': {
-        'resource': ['wood', 'magic_wood'], 
-        'movement': 4 ,
-        'description': 'YOU ARE IN THE DEEP, DARK FOREST',
-        'img_color' : ImageColor.getrgb('darkgreen'),
-        'symbol' : '♣',     #'▒',
-        'format' : {'fgcolor':'LIGHTGREEN_EX','bgcolor':'BLACK','style':'NORMAL'},
-        },
-    'river': {
-        'resource' : ['water', 'fish'],
-        'movement' : 3 ,
-        'description' : 'YOU ARE IN A RIVER',
-        'img_color' : ImageColor.getrgb('cyan'),
-        'symbol' : '~',
-        'format' : {'fgcolor':'BLUE','bgcolor':'CYAN','style':'NORMAL'},
-        },
-    'lake': {
-        'resource' : ['water', 'fish'],
-        'movement' : 4 ,
-        'description' : 'YOU ARE ON A LAKE',
-        'img_color' : ImageColor.getrgb('darkcyan'),
-        'symbol' : '~',
-        'format' : {'fgcolor':'CYAN','bgcolor':'BLUE','style':'NORMAL'},
-        },
-    'swamp': {
-        'resource': None, 
-        'movement': 4 ,
-        'description': 'YOU ARE IN THE SWAMP',
-        'img_color' : ImageColor.getrgb('darkolivegreen'),
-        'symbol' : '▒',
-        'format' : {'fgcolor':'RED','bgcolor':'GREEN','style':'NORMAL'},
-        },
-    'salt_marsh': {
-        'resource': ['fish', 'salt'], 
-        'movement': 3 ,
-        'description': 'YOU ARE IN THE SALT MARSH',
-        'img_color' : ImageColor.getrgb('darkseagreen'),
-        'symbol' : '▒',
-        'format' : {'fgcolor':'GREEN','bgcolor':'BLUE','style':'NORMAL'},
-        },
-    'alpine_grassland': {
-        'resource': ['pine'], 
-        'movement': 2 ,
-        'description': 'YOU ARE IN THE ALPINE GRASSLAND',
-        'img_color' : ImageColor.getrgb('lightgreen'),
-        'symbol' : '▒',
-        'format' : {'fgcolor':'WHITE','bgcolor':'LIGHTGREEN_EX','style':'NORMAL'},
-        },
-    ## -------------------------------------------------------------------------
-    ## impassable boundary biomes
-    ## -------------------------------------------------------------------------    
-    'mountain': {
-        'resource': None,
-        'movement': -1 ,
-        'description': 'YOU ARE IN THE MOUNTAINS',
-        'img_color' : ImageColor.getrgb('darkslategray'),
-        'symbol' : '▲',     #'^',
-        'format' : {'fgcolor':'WHITE','bgcolor':'LIGHTBLACK_EX','style':'NORMAL'},
-        },
-    'ocean': {
-        'resource': None, 
-        'movement': -1 ,
-        'description': 'YOU ARE IN THE OCEAN',
-        'img_color' : ImageColor.getrgb('blue'),
-        'symbol' : '~',
-        'format' : {'fgcolor':'BLACK','bgcolor':'BLUE','style':'NORMAL'},
-        },
-    }
+    'village' : Tile('village', movement=1, color='red', symbol='±',
+        description='YOU ARE IN A PEACEFUL VILLAGE',
+        resources=None,
+        _format={'fgcolor':'BLACK','bgcolor':'WHITE','style':'NORMAL'},
+        ),
+    'cave' : Tile('cave', movement=1, color='black', symbol='▄',
+        description='YOU ARE STANDING AT THE ENTRANCE TO A DEEP, DARK CAVE',
+        resources=None,
+        _format={'fgcolor':'BLACK','bgcolor':'LIGHTBLACK_EX','style':'NORMAL'},
+        ),
+    'road' : Tile('road', movement=1, color='white', symbol='·',
+        description='YOU ARE IN A VILLAGE',
+        resources=None,
+        _format={'fgcolor':'BLACK','bgcolor':'LIGHTBLACK_EX','style':'NORMAL'},
+        ),
+    'farmland' : Tile('farmland', movement=2, color='magenta', symbol='≡',
+        description='YOU ARE IN A VILLAGE',
+        resources=['herb'],
+        _format={'fgcolor':'LIGHTYELLOW_EX','bgcolor':'GREEN','style':'NORMAL'},
+        ),
+    'beach' : Tile('beach', movement=2, color='khaki', symbol='░',
+        description='YOU ARE IN A VILLAGE',
+        resources=None,
+        _format={'fgcolor':'WHITE','bgcolor':'YELLOW','style':'NORMAL'},
+        ),
+    'desert' : Tile('desert', movement=3, color='peru', symbol='░',
+        description='YOU ARE IN A VILLAGE',
+        resources=None,
+        _format={'fgcolor':'RED','bgcolor':'YELLOW','style':'NORMAL'},
+        ),
+    'grassland' : Tile('grassland', movement=2, color='lawngreen', symbol=' ',
+        description='YOU ARE IN A VILLAGE',
+        resources=None,
+        _format={'fgcolor':'LIGHTYELLOW_EX','bgcolor':'LIGHTGREEN_EX','style':'NORMAL'},
+        ),
+    'forest' : Tile('forest', movement=3, color='olivedrab', symbol='♣',
+        description='YOU ARE IN THE FOREST',
+        resources=['wood'],
+        _format={'fgcolor':'GREEN','bgcolor':'LIGHTGREEN_EX','style':'NORMAL'},
+        ),
+    'deep_forest' : Tile('deep_forest', movement=4, color='darkgreen', symbol='♣',
+        description='YOU ARE IN THE DEEP, DARK FOREST',
+        resources=None,
+        _format={'fgcolor':'LIGHTGREEN_EX','bgcolor':'BLACK','style':'NORMAL'},
+        ),
+    'river' : Tile('river', movement=2, color='cyan', symbol='~',
+        description='YOU ARE ON THE RIVER',
+        resources=['water', 'fish'],
+        _format={'fgcolor':'BLUE','bgcolor':'CYAN','style':'NORMAL'},
+        ),
+    'lake' : Tile('lake', movement=3, color='darkcyan', symbol='~',
+        description='YOU ARE ON A LAKE',
+        resources=['water', 'fish'],
+        _format={'fgcolor':'CYAN','bgcolor':'BLUE','style':'NORMAL'},
+        ),
+    'swamp' : Tile('swamp', movement=5, color='darkolivegreen', symbol='▒',
+        description='YOU ARE IN THE SWAMP',
+        resources=None,
+        _format={'fgcolor':'RED','bgcolor':'GREEN','style':'NORMAL'},
+        ),
+    'salt_marsh' : Tile('salt_marsh', movement=4, color='darkseagreen', symbol='▒',
+        description='YOU ARE IN THE SALT MARSH',
+        resources=['fish', 'salt'],
+        _format={'fgcolor':'GREEN','bgcolor':'BLUE','style':'NORMAL'},
+        ),
+    'alpine_grassland' : Tile('alpine_grassland', movement=1, color='lightgreen', symbol='░',
+        description='YOU ARE IN THE ALPINE GRASSLAND',
+        resources=['pine'],
+        _format={'fgcolor':'WHITE','bgcolor':'LIGHTGREEN_EX','style':'NORMAL'},
+        ),
+    'mountain' : Tile('mountain', movement=-1, color='darkslategray', symbol='^',
+        description='YOU ARE IN THE MOUNTAINS',
+        resources=None,
+        _format={'fgcolor':'WHITE','bgcolor':'LIGHTBLACK_EX','style':'NORMAL'},
+        ),
+    'ocean' : Tile('ocean', movement=-1, color='blue', symbol='~',
+        description='YOU ARE IN THE OCEAN',
+        resources=None,
+        _format={'fgcolor':'BLACK','bgcolor':'BLUE','style':'NORMAL'},
+        ),
+
+}
