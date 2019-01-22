65844758038fabbc727389e4a70bd441f9bc7ac4
Chris Bruce <chris.bruce@dsvolition.com>
2019-01-22 14:14:06 -0600

    added more test functions to main function

--- test_map.py
+++ test_map.py
@@ -7,20 +7,30 @@ from WordRPG.Map.tiles import BIOMES
 
 
 
-# get list of .txt filenames in the 'screens' subfolder
+# load map image from relative folder
 DIRNAME = os.path.dirname(__file__)
 FILEPATH = os.path.join(DIRNAME, 'WordRPG', 'Map', 'images')
 TEST_MAP = os.path.join(FILEPATH, 'test_island2.png')
 
 
 if __name__ == '__main__':
-    # sets up terminal window to expected size
-    gui.Screen.setup_terminal(title='MAP TEST', size=(100,50))
+    # test data in BIOMES tileset
+    # print(BIOMES)
+    # for k, v in BIOMES.items():
+    #     print(k, v)
 
     # create a 'Map' object using the given image filename and then print
     # it to the terminal
-    test_map = Map(TEST_MAP)
-    test_map.show()
+    test_world = Map(TEST_MAP)
+
+    # sets up terminal window to expected size
+    gui.Screen.setup_terminal(title='MAP TEST', size=(100,50))
+
+    # # prints 2D array of tile symbls
+    # print(test_map)
+
+    # # prints the map to the terminal with full formatting
+    test_world.show()
 
     # adding while True loop so terminal stays open until manually closed
     while True:
