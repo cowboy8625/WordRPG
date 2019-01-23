cd61a606f31cc9f8793bcfc9aa96f34bfb0db4cc
ChristopherLBruce <37425633+ChristopherLBruce@users.noreply.github.com>
2019-01-23 00:55:31 -0600

    procedural symbols for 'paths'

--- WordRPG/map/map.py
+++ WordRPG/map/map.py
@@ -1,4 +1,5 @@
 from sys import stdout
+from copy import deepcopy
 
 from PIL import ImageColor, Image, ImageOps
 
@@ -36,7 +37,7 @@ class Map:
         self.map_key = self.get_map_key(tileset=self.tileset)
         self.cols, self.rows = self.size = (0, 0)
         self.map = self._load_map(filename)
-        self.display_map = self._update_display_map()
+        self.display_map = self._get_display_map()
 
 
     def get_map_key(self, tileset=BIOMES):
@@ -55,12 +56,8 @@ class Map:
         Arguments:
             filename {[type]} -- [description]
 
-        Keyword Arguments:
-            mirror {bool} -- [description] (default: {True})
-            rotate {int} -- [description] (default: {90})
-
         Returns:
-            [type] -- [description]
+            [list] -- 'map' array, list of lists containing 'Tile' objects
         """
 
         image = Image.open(filename)
@@ -71,9 +68,11 @@ class Map:
         # generate color key used to parse map image into a 2D array of tile dicts
         map_key = self.get_map_key()
 
-        # image data is collected row by row, so pixels end up being stored in
+        # collect image data as a list of rgb pixel values
         pixels = [x for x in list(image.getdata())]
 
+        # loop through list of pixels and convert them to appropriate tile
+        # based on the tileset
         tiles = []
         for rgb in pixels:
             if rgb not in self.map_key:
@@ -81,19 +80,95 @@ class Map:
                 tiles.append(None)
             else:
                 tile_key = self.map_key[rgb]
-                # TODO: this might be creating a pointer to the 'tileset' dict
-                # we might need to instantiate a new 'Tile' object here instead
-                tiles.append(self.tileset[tile_key])
-
-        # tiles = [self.tileset[self.map_key[rgb]] for rgb in pixels]
-
-        # converts long list of tiles into 2D array (list of lists)
+                # TODO: not sure if it's better to do a deepcopy of an already
+                # instantiated class or if we should create the objects one at
+                # a time here
+                tiles.append(deepcopy(self.tileset[tile_key]))
+        
+        # converts list of tiles into 'map' array (2D list of lists)
         _map = [tiles[i:(i + self.cols)] for i in range(0, len(tiles), self.cols)]
 
         return _map
 
 
-    def get_map_segment(self, size, offset=(0,0)):
+    def _get_tile(self, pos):
+        """ Gets the tile at the given pos in the map array
+
+        Arguments:
+            pos {tuple} -- (col,row) position in the map array
+        
+        Returns:
+            [type] -- [description]
+        """
+
+        col, row = pos
+        try:
+            return self.map[row][col]
+        except IndexError:
+            return None
+
+
+    def _get_neighbors(self, pos):
+        """ Gets neighboring tiles at given position in map array
+        
+        Arguments:
+            pos {tuple} -- (col,row) position in the map array
+        
+        Returns:
+            tuple -- tiles above, right, below, and left of the given pos
+        """
+
+        col, row = pos
+        t = self._get_tile((col, row - 1))
+        r = self._get_tile((col + 1, row))
+        b = self._get_tile((col, row + 1))
+        l = self._get_tile((col - 1, row))
+
+        return t, r, b, l
+
+
+    def _get_path_symbol(self, pos):
+        """[summary]
+        
+        Arguments:
+            pos {tuple} -- (col,row) position in the map array
+        
+        Returns:
+            [str] -- Symbol to use for the path tile at the given position
+        """
+
+        # this uses a binary value as a psuedo bit-flag to determine which
+        # symbol should be used to draw a 'path' tile so that it connects to all
+        # neighboring 'path' tiles
+        char = {0:'═', 1:'║', 2:'║', 3:'║', 4:'═', 5:'╝', 6:'╗' ,7:'╣', 8:'═',
+                9:'╚', 10:'╔', 11:'╠', 12:'═', 13:'╩', 14:'╦', 15:'╬'}
+
+        t, r, b, l = self._get_neighbors(pos)
+        key = 0
+        if t is not None and t.name == 'path':
+            key += 1
+        if b is not None and b.name == 'path':
+            key += 2
+        if l is not None and l.name == 'path':
+            key += 4
+        if r is not None and r.name == 'path':
+            key += 8
+        
+        return char[key]
+
+
+    def _connect_path_tiles(self):
+        """ Connect 'path' tiles with appropriate symbol """
+
+        for row in range(len(self.map)):
+            for col in range(len(self.map[row])):
+                tile = self.map[row][col]
+                if tile.name == 'path':
+                    tile.symbol = self._get_path_symbol((col,row))
+                    tile.char = tile._get_formatted_char()
+
+
+    def get_map_slice(self, size, offset=(0,0)):
         """ Gets a portion of the map array
 
         Gets a portion of the map array so it can be displayed in the game window
@@ -103,23 +178,40 @@ class Map:
             size {tuple} -- number of (rows, cols) of the map to return
             offset {tuple} -- starting (row,col) of the segment
 
-        Notes:
-            Need to account for the window being positioned in a way that attempts
-            to get out of range data
+        ToDo:
+            Need to account for the window being positioned in a way that
+            attempts to get out of range data. Should return a default 'Tile'
+            object, such as 'water' or 'mountain'
         """
+        start_col, start_row,  = offset
+        cols, rows = size
 
-        pass
+        map_slice = []
+        for row in range(start_row, start_row + rows):
+            line = []
+            for col in range(start_col, start_col + cols):
+                try:
+                    line.append(self.map[row][col])
+                except IndexError:
+                    line.append(None)
+            map_slice.append(line)
+
+        return map_slice
 
 
-    def _update_display_map(self, discovered=False):
+    def _get_display_map(self, _map=None, discovered=False):
         """ Creates the formatted map for display
 
         Keywords:
+            _map {list} -- 'map' array to display
             discovered {bool} -- If True, only show formatted character for
                                  tiles that have been 'discovered'.
                                  Default is False.
 
         """
+
+        if _map is None:
+            _map = self.map
 
         # Go through each row in self.map array and get all of the Tile.char
         # properties (formatted symbol). If the tile has a value of None or the
@@ -131,22 +223,42 @@ class Map:
                         [tile.char if tile is not None and \
                         ( not discovered or tile.discovered) \
                         else ' ' for tile in row] ) \
-                        for row in self.map]
+                        for row in _map]
         return '\n'.join(rows)
 
 
-    def show(self, discovered=False):
-        """ Prints the map array """
+    def show(self, _map=None, discovered=False):
+        """ Prints the map array to terminal """
 
-        if discovered:
-            self._update_display_map(discovered=True)
+        if _map is None:
+            _map = self.map
 
-        return stdout.write(self.display_map)
+        map_string = self._get_display_map(_map=_map, discovered=discovered)
+        return stdout.write(map_string)
 
 
     def __repr__(self):
         """ returns explicit representation of self.map data """
         return self.map
+
+
+    def _map_to_string(self, _map):
+        """ Converts 'map' array to string of tile symbols
+        
+        Arguments:
+            _map {list} -- 'map' array
+        
+        Note:
+            This was broken out into a seperate function from __str__ so that
+            it can be used to print map slices from .get_map_slice as well as
+            the full self.map
+
+        Returns:
+            [str] -- string of tile symbols
+        """
+
+        rows = [''.join([tile.symbol for tile in row]) for row in _map]
+        return '\n'.join(rows)
 
 
     def __str__(self):
@@ -155,5 +267,4 @@ class Map:
         # Go through each row in self.map array and get all of the Tile.symbol
         # characters.
         # Then join all of the rows into a single string which can be printed
-        rows = [''.join([tile.symbol for tile in row]) for row in self.map]
-        return '\n'.join(rows)
+        self._map_to_string(self.map)
