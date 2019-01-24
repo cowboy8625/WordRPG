17ffae44a28a373892980980bfe58e93867e0920
ChristopherLBruce <37425633+ChristopherLBruce@users.noreply.github.com>
2019-01-24 00:51:48 -0600

    updated with new tests

--- test_map.py
+++ test_map.py
@@ -18,30 +18,44 @@ TEST_MAP = os.path.join(FILEPATH, 'test_island2.png')
 
 
 if __name__ == '__main__':
-    # test data in BIOMES tileset
+    """test data in BIOMES tileset"""
     # print(BIOMES)
     # for k, v in BIOMES.items():
     #     print(k, v)
-
-    # create a 'Map' object using the given image filename and then print
-    # it to the terminal
+ 
+    """ create a 'Map' object using the given image filename and then print
+    it to the terminal """
     test_world = Map(TEST_MAP)
 
-    # # prints 2D array of tile symbls
-    # print(test_world)
+    """ prints map as raw tile symbols """
+    gui.screen.setup_terminal(title='MAP TEST', size=test_world.size)
+    print(test_world)
+    sleep(2)
 
-    # sets up terminal window to expected size
-    gui.screen.setup_terminal(title='MAP TEST', size=(100,50))
-    # prints the map to the terminal with full formatting
+    """ sets up terminal window to expected size and prints the map to the
+    terminal with full font formatting """
+    gui.screen.setup_terminal(title='MAP TEST', size=test_world.size)
     test_world.show()
+    sleep(2)
 
-    # test map slicing
-    # size = (40,20) # rows, cols
-    # start = (20,10)
-    # gui.screen.setup_terminal(title='MAP TEST', size=size)
-    # map_slice = test_world.get_map_slice(size, offset=start)
-    # test_world.show(_map=map_slice)
+    """ test map frame """
+    size = (40, 20)
+    offset = (15, 15)
+    test_world.set_map_frame(size, offset=offset)
+    gui.screen.setup_terminal(title='MAP FRAME TEST', size=size)
+    test_world.show(frame=True, raw=False)
+    sleep(2)
 
-    # adding while True loop so terminal stays open until manually closed
+    """ test scrolling map frame """
+    size = (30, 15)
+    gui.screen.setup_terminal(title='MAP FRAME TEST', size=size)
+    for r in range(0, 30, 15):
+        for c in range(0, 80, 1):
+            offset = (c, r)
+            test_world.set_map_frame(size, offset=offset)
+            test_world.show(frame=True, clear_first=True)
+            sleep(1.0 / 15)
+
+    """adding while True loop so terminal stays open until manually closed"""
     while True:
         pass
