6001600a9e278ca1f36857736d0e8c4434799c8b
ChristopherLBruce <37425633+ChristopherLBruce@users.noreply.github.com>
2019-01-27 22:52:49 -0600

    added SETTINGS to root 'const' file

--- WordRPG/const.py
+++ WordRPG/const.py
@@ -2,3 +2,14 @@ NAME = 'WASTELANDS'
 VERSION = 0.2
 YEAR = 2019
 COMPANY = 'COWBOY GAMING'
+
+
+# TODO: starting to implement a settings menu for development and game options
+# eventually this should be managed as an extrnal .json file so we can save/load
+# them in game
+SETTINGS = {
+    # 'start_state' : 'main_menu',        #'game',
+    'start_state' : 'game',
+    'symbol' : True,
+    'color' : True,
+}
