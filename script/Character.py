eab449842d9887068f4f41cad4144f28099771e9
Byteme8bit <lynchpcconsulting@gmail.com>
2019-01-27 18:29:35 -0800

    Character class revamping
    > Stripped down file to utter basics to restructure classes
    >> Character is baseline class for all entities
    >> Player is user controlled entity with utter basics currently
    >> Mob is baseline class for all combat entities
    >> NPC is baseline class for all non combat entities

--- script/Character.py
+++ script/Character.py
@@ -1,171 +1,41 @@
-# Program imports
-import random
+__author__ = 'byteme8bit'
 
-from script import Items
-from Mechanics.ui_mechanics import *
-from Map_Gen.ParseMap import WorldMap
+# File imports
+
+# Module imports
 
 
-class Character:
-    def __init__(self, name, max_health, melee_attack, magic_attack,
-                 max_mana, max_stamina, defense, luck, level=1):
-        self.name = name
+class Character:        # Base class
+    def __init__(self, name, level):
+        self.char_name = name
         self.level = level
-        # Stats
-        self.max_health = max_health
-        self.health = self.max_health
-        self.melee_damage = melee_attack
-        self.magic_damage = magic_attack
-        self.max_mana = max_mana
-        self.max_stamina = max_stamina
-        self.defense = defense
-        self.luck = luck
 
-        # Inventory
-        self.equipped_weapon = Items.fist
-        self.equipped_armor = None
+
+class Player(Character):        # User's entity
+    def __init__(self, name, level, health_points, char_class):
+        super().__init__(name, level)
+        self.type = char_class
+        self.exp = 0
+        self.hp = health_points
 
     def melee_attack(self):
-        return 17           # TODO Formula will be used after testing
-        # return random.randint(self.melee_damage // 2, self.melee_damage)
+        return 17
 
     def magic_attack(self):
-        return 17           # TODO Formula will be used after testing
-        # return random.randint(self.magic_damage // 2, self.magic_damage)
-
-
-class Player(Character):
-    def __init__(self, name, _class, max_health, melee_attack, magic_attack,
-                 max_mana, max_stamina, defense, luck, gender, gold=0, xp=0,
-                 equipped_weapon=None, equipped_armor=Items.farm_clothing):
-        super().__init__(name, max_health, melee_attack, magic_attack,
-                         max_mana, max_stamina, defense, luck)
-        self._class = _class
-
-        if self._class == 'Warrior':
-            self.strength = int(15 + (self.level * 4.2))
-            self.agility = int(13 + (self.level * 3.7))
-            self.intelligence = int(11 + (self.level * 2.8))
-
-        elif self._class == 'Assassin':
-            self.strength = int(13 + (self.level * 3.5))
-            self.agility = int(15 + (self.level * 4.2))
-            self.intelligence = int(11 + (self.level * 2.9))
-
-        elif self._class == 'Archer':
-            self.strength = int(13 + (self.level * 3.7))
-            self.agility = int(15 + (self.level * 4))
-            self.intelligence = int(11 + (self.level * 2.9))
-
-        elif self._class == 'Mage':
-            self.strength = int(13 + (self.level * 2.8))
-            self.agility = int(11 + (self.level * 3.7))
-            self.intelligence = int(15 + (self.level * 4.2))
-
-        # If player is none type
-        else:
-            self.strength = int(13 + (self.level * 3.5))
-            self.agility = int(13 + (self.level * 3.5))
-            self.intelligence = int(13 + (self.level * 3.5))
-
-        self.max_health = 100
-        self.gender = gender
-        self.gold = gold
-        self.xp = xp
-        self.equipped_weapon = equipped_weapon
-        self.equipped_armor = equipped_armor
-        self.died_count = 0
-
-        self.inventory = [Items.flint, Items.water]
-        # start with a couple of things so we can play with inventory management
-        self.inventory_limit = 10
-
-        # Map position
-        self.pos_x = 1
-        self.pos_y = 1
+        pass
 
     def health_generation(self):
-        """
-        Determines maximum amount of health
-        :return: Max. health
-        """
+        pass
 
-        # If ranger
-        if self._class == 'Assassin' or self._class == 'Archer':
-            return ((100 * self.level) + (self.strength * 1.7) // (1.25 * self.level)) * 0.97
 
-        # If warrior
-        elif self._class == 'Warrior':
-            return ((100 * self.level) + (self.strength * 2) // (1.1 * self.level)) * 1.15
+class Mob(Character):       # Combat based entities
+    def __init__(self, name, level, health_points):
+        super().__init__(name, level)
+        self.hp = health_points
 
-        # If mage
-        elif self._class == 'Mage':
-            return ((100 * self.level) + (self.strength * 1.4) // (1.35 * self.level)) * 0.89
+    def melee_attack(self):
+        return 5
 
-        # If none type
-        else:
-            return (100 * self.level) + (self.strength * 1.2) // (1.35 * self.level) * 0.85
 
-    def health_regeneration(self):
-        """
-        Logic responsible for health regeneration
-        :return: None
-        """
-        max_hp = self.health_generation()       # Sets value of max. health
-
-        if self.health < max_hp:        # If current health is less than max. health
-            self.health += (max_hp * 0.023)         # Rate of regeneration
-
-    def player_fainted(self):
-        """
-        Defines what happens when Player health reach <= 0
-        :return: None
-        """
-
-        self.died_count += 1
-        print(f"{self.name} has fainted and needs to recover...\n")
-        # time.sleep(5)
-        # self.resource_reset()
-        print(f'{self.name} has recovered! {self.name} and has fallen {self.died_count} times.\n')
-
-    def move(self, _dir):
-        directions = {"North": (0, -1), "South": (0, 1), "East": (1, 0), "West": (-1, 0)}
-        delta_x, delta_y = directions[_dir]
-        new_x, new_y = self.pos_x + delta_x, self.pos_y + delta_y
-        if WorldMap.access_information(new_x, new_y, "Crossable"):
-            self.pos_x, self.pos_y = new_x, new_y
-        else:
-            clear()
-            print("You cannot cross here:")
-            print(WorldMap.access_information(new_x, new_y, "Name"))
-            pause()
-
-    # Add item to inventory if space is available
-    # Return true if add was successful
-    def add_to_inv(self, item):
-        if len(self.inventory) >= self.inventory_limit:
-            return False
-        self.inventory.append(item)
-
-    # Remove item at given position
-    def remove_from_inv(self, index):
-        if index in range(len(self.inventory)):
-            del self.inventory[index]
-        else:
-            raise ValueError(f'Index {index} invalid for inventory {self.inventory}')
-
-    # TODO methods like this should probably be moved to mechanics
-    # the script classes shouldn't be dealing with UI, only handling the class instances
-    def inspect_area(self):
-        info = {
-            "Name": WorldMap.access_information(self.pos_x, self.pos_y, "Name"),
-            "Resources": WorldMap.access_information(self.pos_x, self.pos_y, "Resources"),
-            "Spawns": WorldMap.access_information(self.pos_x, self.pos_x, "Spawns"),
-            "Info": WorldMap.access_information(self.pos_x, self.pos_y, "Info")
-        }
-        clear()
-        print("Name: " + str(info["Name"]))
-        print("Resources: " + str(info["Resources"]))
-        print("Spawns: " + str(info["Spawns"]))
-        print("Info: " + str(info["Info"]))
-        pause()
+class NPC(Character):       # Non-combat entities
+    pass
