2f9cafc0544a28699a888525bd24af78dfa1cb4c
ChristopherLBruce <37425633+ChristopherLBruce@users.noreply.github.com>
2019-02-02 15:37:03 -0600

    renamed FSM to Machine

--- WordRPG/main.py
+++ WordRPG/main.py
@@ -1,13 +1,13 @@
 import sys
 
-from .state_machine.machine import FSM
+from .state_machine.machine import Machine
 from .data.states import STATES
 from .gui.screen import Screen
 
 
 
 def run(start='main_menu'):
-    game = FSM(states=STATES,start=start)
+    game = Machine(states=STATES,start=start)
 
     Screen.setup_terminal()
 
